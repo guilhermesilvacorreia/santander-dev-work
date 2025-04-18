ec596e4b3b032c87ef2e7ef4b50db4bb3cf5fc7f
guilhermesilvacorreia <guilhermecorreiasilva29@gmail.com>
2025-04-18 15:30:09 -0300

    subindo projeto

---
+++ src/main/java/bootcamp/santander_dev_work/Application.java
@@ -0,0 +1,14 @@
+package bootcamp.santander_dev_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevWorkApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/bootcamp/santander_dev_work/SantanderDevWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.santander_dev_work;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderDevWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
